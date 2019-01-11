f593172bf13d3c8871c95ab254e5c2a458ce5bce
WangWang <luckboystudent@gmail.com>
2019-01-11 10:42:46 +0800

    start up the proxy.js

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const { exec } = require('child_process');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var cors = require('cors');
 var app = express();
+
+exec(`node proxy.js`, (error, stdout, stderr) => {})
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
